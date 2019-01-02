6b52261e4a2a0cb9c0dc2094f402698c274f76d4
Mike North <michael.l.north@gmail.com>
2019-01-01 22:31:31 -0800

    chore: create CLI module

---
+++ packages/cli/README.md
@@ -0,0 +1,11 @@
+# `@snap-doc/cli`
+
+> TODO: description
+
+## Usage
+
+```
+const snapDoc = require('snap-doc');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/cli/__tests__/snap-doc.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const snapDoc = require('..');
+
+describe('snap-doc', () => {
+  it('needs tests');
+});
