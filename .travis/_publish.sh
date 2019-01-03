744c54ad6e8c7692a4a0dc9fce3432614efc44f9
Mike North <michael.l.north@gmail.com>
2019-01-03 12:57:54 -0800

    ci: fix publish script

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-
+npm install -g lerna
 echo "On master branch. Proceeding with publish"
-echo "git status"
-git status
-echo "lerna publish"
-npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
-echo "npm whoami"
-npm whoami
-git checkout $TRAVIS_BRANCH
-echo "git status"
-git status
+rm -rf .git
+git init
+git clean -dfx
+git remote add origin https://github.com/snap-doc/snap-doc.git
+git fetch origin
+git clone "https://github.com/$TRAVIS_REPO_SLUG.git" "$TRAVIS_REPO_SLUG"
+git checkout "$TRAVIS_BRANCH"
 git config credential.helper store
+echo "https://mike-north:${GH_TOKEN}@github.com/mike-north/snap-doc.git" > ~/.git-credentials
+
+npm config set "//registry.npmjs.org/:_authToken=$NPM_TOKEN" -q
+npm prune
+
 git config --global user.email "michael.l.north@gmail.com"
 git config --global user.name "Mike North"
 git config --global push.default simple
-echo "https://mike-north:${GH_TOKEN}@github.com/snap-doc/snap-doc.git" > ~/.git-credentials
+
+git fetch --tags
+git branch -u "origin/$TRAVIS_BRANCH"
+git fsck --full #debug
+
+echo "npm whoami"
+npm whoami
 echo "git config --list"
 git config --list #debug
-./node_modules/.bin/lerna publish --yes
+
+echo "lerna publish"
+lerna publish --yes
