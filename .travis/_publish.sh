f60cdb86211fe99c681d69e22c6358b6bae05f2d
Mike North <michael.l.north@gmail.com>
2019-01-04 22:04:51 -0800

    fix: roll back to old publish script

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -1,46 +1,20 @@
 #!/bin/bash
-npm install -g lerna
-echo "On master branch. Proceeding with publish"
-rm -rf .git
-git init
-git clean -dfx
-git remote add origin https://github.com/snap-doc/snap-doc.git
-git fetch origin
-git clone "https://github.com/$TRAVIS_REPO_SLUG.git" "$TRAVIS_REPO_SLUG"
-git checkout "$TRAVIS_BRANCH"
-git config credential.helper store
-echo "https://mike-north-bot:${GH_TOKEN}@github.com/snap-doc/snap-doc.git" > ~/.git-credentials
 
-npm config set "//registry.npmjs.org/:_authToken=$NPM_TOKEN" -q
-npm prune
+echo "On master branch. Proceeding with publish"
+echo "git status"
+git status
+echo "lerna publish"
+npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
 echo "npm whoami"
 npm whoami
-
+git checkout $TRAVIS_BRANCH
+echo "git status"
+git status
+git config credential.helper store
 git config --global user.email "michael.l.north@gmail.com"
 git config --global user.name "Mike North"
 git config --global push.default simple
-
-git fetch --tags
-git branch -u "origin/$TRAVIS_BRANCH"
-git fsck --full #debug
-
-echo "npm whoami"
-npm whoami
+echo "https://mike-north:${GH_TOKEN}@github.com/snap-doc/snap-doc.git" > ~/.git-credentials
 echo "git config --list"
 git config --list #debug
-
-yarn install --non-interactive
-lerna bootstrap
-yarn build
-
-echo "git status"
-git status
-
-ls -all .
-ls -all snap-doc/snap-doc
-
-echo "npm whoami"
-npm whoami
-
-echo "lerna publish"
-lerna publish --yes
+./node_modules/.bin/lerna publish --yes
