92de3b8e81865cb62e56353854ca613c31b2dbac
Mike North <michael.l.north@gmail.com>
2019-01-13 10:59:43 -0800

    chore: updates for breaking code-to-json api changes

--- packages/core/src/doc-data.ts
+++ packages/core/src/doc-data.ts
@@ -2,7 +2,11 @@ import { FormatterOutput } from '@code-to-json/formatter';
 import { DocData, DocDataFile } from '@snap-doc/types';
 
 export function generateDocData(input: FormatterOutput): DocData {
-  const files: DocDataFile[] = input.sourceFiles.map(f => {
+  const {
+    data: { sourceFiles }
+  } = input;
+  const files: DocDataFile[] = Object.keys(sourceFiles).map(fname => {
+    const f = sourceFiles[fname];
     const { moduleName, pathInPackage, documentation } = f;
     return {
       name: moduleName,
