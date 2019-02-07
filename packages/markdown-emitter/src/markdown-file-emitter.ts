ed8dba4ea7f7d0347a0015b8043db9414f469e14
Mike North <michael.l.north@gmail.com>
2019-02-06 18:40:15 -0800

    feat: option to overwrite existing output dir

--- packages/markdown-emitter/src/markdown-file-emitter.ts
+++ packages/markdown-emitter/src/markdown-file-emitter.ts
@@ -3,6 +3,7 @@ import {
   LinkedFormattedSourceFile
 } from '@code-to-json/formatter-linker';
 import { isDefined } from '@code-to-json/utils';
+import { SysHost } from '@code-to-json/utils-ts';
 import { FileEmitter, FileEmitterOptions } from '@snap-doc/emitter';
 import * as debug from 'debug';
 import { markdownForDocFile } from './md/utils';
@@ -13,40 +14,51 @@ const log = debug('snap-doc:markdown-file-emitter');
 export interface MarkdownFileEmitterOptions extends FileEmitterOptions {}
 
 export default class MarkdownFileEmitter extends FileEmitter<MarkdownFileEmitterOptions> {
-  constructor(options: MarkdownFileEmitterOptions) {
-    super(options);
-    log('outDir', this.options.outDir);
+  constructor(host: SysHost, options: MarkdownFileEmitterOptions) {
+    super(host, options);
   }
 
   public async generate(data: LinkedFormattedOutputData): Promise<void> {
     const { sourceFiles: files } = data;
-    const { outDir, host: h } = this.options;
-    const outExists = h.fileOrFolderExists(outDir);
-    if (outExists && !h.isFolder(outDir)) {
-      throw new Error(`Path ${outDir} exists, but is not a directory`);
-    } else if (outExists) {
-      log(`Deleting existing contents at ${outDir}`);
-      await h.removeFolderAndContents(outDir);
+    const { outDir } = this.options;
+
+    // Ensure that the
+    if (this.host.fileOrFolderExists(outDir)) {
+      if (!this.host.isFolder(outDir)) {
+        throw new Error(`Path ${outDir} exists, but is not a directory`);
+      } else if (this.options.overwriteOutDir) {
+        log(`Deleting existing contents at ${outDir}`);
+        await this.host.removeFolderAndContents(outDir);
+      } else {
+        throw new Error(
+          `Existing content found at ${outDir}. 
+Please use either
+  (1) the 'overwriteOutDir' API option or 
+  (2) the '--force' CLI flag 
+if you want to replace existing content in the output directory`
+        );
+      }
     }
+
     log(`Creating new directory at ${outDir}`);
-    h.createFolder(outDir);
+    this.host.createFolder(outDir);
 
     Object.keys(files)
       .map(fname => files[fname])
       .filter(isDefined)
       .filter(f => !f.isDeclarationFile)
       .forEach(f => {
-        const outPath = h.combinePaths(this.options.outDir, f.path);
+        const outPath = this.host.combinePaths(this.options.outDir, f.path);
         log(`Processing module: ${f.moduleName} (${f.path})`);
-        const parentDir = h.combinePaths(outPath, '..');
-        if (!h.fileOrFolderExists(parentDir)) {
-          h.createFolder(parentDir);
+        const parentDir = this.host.combinePaths(outPath, '..');
+        if (!this.host.fileOrFolderExists(parentDir)) {
+          this.host.createFolder(parentDir);
         }
-        if (!h.isFolder(parentDir)) {
+        if (!this.host.isFolder(parentDir)) {
           throw new Error(`${parentDir} is not a directory`);
         }
         const content = this.contentForModule(data, f);
-        h.writeFileSync(`${outPath}.md`, content);
+        this.host.writeFileSync(`${outPath}.md`, content);
       });
   }
 
