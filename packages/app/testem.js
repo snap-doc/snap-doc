d05de68bc46e1ca96f874d6a2004e551b31a9b83
Mike North <michael.l.north@gmail.com>
2019-04-08 16:45:44 -0700

    Azure Pipelines Config (#284)
    * ci(azure): refactor pipelines config
    
    * ci(azure): build artifact publish
    
    * ci(azure): lint job
    
    * ci(azure): ember-try scenarios
    
    * ci(azure): increase browser startup timeout
    
    * ci(azure): code coverage support
    
    * ci(azure): start ember-try jobs once linux_fixed completes
    
    * chore: fix type-checkign error in markdown emitter tests

--- packages/app/testem.js
+++ packages/app/testem.js
@@ -3,21 +3,30 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
+  reporter: 'xunit',
+  report_file: 'ember-tests.xml',
+  xunit_exclude_stack: true, // we *probably* want this on to keep the xunit file clean
+  xunit_intermediate_output: true,
+  launch_in_ci: process.env.NO_FIREFOX ? ['Chrome'] : ['Chrome', 'Firefox'],
   launch_in_dev: ['Chrome'],
+  browser_start_timeout: 60000,
   browser_args: {
+    Firefox: { ci: ['-headless', '--window-size=1440,900'] },
     Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
+      mode: 'ci',
+      // prettier-disable -- args are useful to have in a sane order
+      args: [
         '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
+        '--headless',
         '--remote-debugging-port=0',
         '--window-size=1440,900',
-      ].filter(Boolean),
+      ].concat(
+        /*
+          --no-sandbox is needed when running Chrome inside a container.
+          See https://github.com/ember-cli/ember-cli-chai/pull/45/files.
+        */
+        process.env.TRAVIS || process.env.CI ? '--no-sandbox' : [],
+      ),
     },
   },
 };
